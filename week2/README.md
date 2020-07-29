6ffa1c63dd1ef18016493806d1f8d61aaec64981
LucilleN <lucillenjoo@gmail.com>
2020-07-29 04:00:12 -0700

    First pass at script but couldn't get it to work

---
+++ week2/README.md
@@ -0,0 +1,5 @@
+# Week 2
+### Creating A Reference + Applying Animation To Model
+
+- [Screen Recording](MovingRandomObjects_v001_ScreenCapture.mp4)
+- [Script](script_v001.py)
---
+++ week2/createReferenceAndApplyAnim.py
@@ -0,0 +1,140 @@
+import maya.cmds
+import os
+
+##################
+# HELPER METHODS #
+##################
+
+def createNewScene():
+    """
+    Creates a new scene.
+    """
+    maya.cmds.file(new=True, force=True)
+
+def getFileNamespace(filePath):
+    """
+    Given a file path, returns the file name without the 
+    extension, which we will use as the namespace
+    """
+    if not os.path.exists(filePath):
+        maya.cmds.error("File does not exist: {0}".format(filePath))
+        return
+    fileDirectory, fileFullName = os.path.split(filePath)
+    fileName, fileExt = os.path.splitext(fileFullName)
+    return fileName
+
+def createReference(filePath, ns):
+    """
+    Given a file path and a namespace, creates a reference to
+    that file.
+    """
+    if not os.path.exists(filePath):
+        maya.cmds.error("File does not exist: {0}".format(filePath))
+        return
+    maya.cmds.file(filePath, r=True, ns=ns)
+
+def getJointsFromNamespace(ns):
+    """
+    Given a namespace, returns a list of all joints
+    """
+    return maya.cmds.ls("{0}:*".format(ns), type="joint")
+
+def connectSingleAttribute(src, dest, attr):
+    """
+    Given a source object, a destination object, and an 
+    attribute, connects the attribute of the src and dest.
+    """
+    srcString = "{0}.{1}".format(src, attr)
+    destString = "{0}.{1}".format(dest, attr)
+    maya.cmds.connectAttr(srcString, destString, f=True)
+
+def connectTranslateRotateScale(src, dest):
+    """
+    Given a source object and destination object, connects 
+    all translation, rotation, and scale attributes.
+    """
+    attributes = [
+        "translateX", 
+        "translateY",
+        "translateZ",
+        "rotateX",
+        "rotateY",
+        "rotateZ",
+        "scaleX",
+        "scaleY",
+        "scaleZ"
+    ]
+    for attribute in attributes:
+        connectSingleAttribute(src, dest, attribute)
+
+def connectAnimAndRigJoints(animJoints, rigJoints):
+    """
+    Given a list of animation joints and a list of rig joints,
+    connects each pair of joints with translate, rotate, and
+    scale. Note: animJoints and rigJoints should follow the 
+    exact same hierarchy.
+    """
+    for animJoint in animJoints:
+        animJointName = animJoint.split(":")[1]
+        for rigJoint in rigJoints:
+            rigJointName = rigJoint.split(":")[1]
+            if animJointName == rigJointName:
+                connectTranslateRotateScale(animJoint, rigJoint)
+
+def saveFile(newFilePath):
+    """
+    Given a new file path, renames the current file and saves.
+    """
+    maya.cmds.file(rename=newFilePath)
+    maya.cmds.file(save=True, f=True)
+
+def removeReference(filePathToRemove):
+    """
+    Removes the reference to the given file path.
+    """
+    maya.cmds.file(filePathToRemove, rr=True)
+
+##########
+# SCRIPT #
+##########
+
+def main():
+    createNewScene()
+
+    animPath = r'C:\Users\GoodbyeWorld Dev\Documents\Lucille\Tech for Anim\tech-art-exercises\week2\animations\maya\01_01.ma'.replace("\\", "/")
+    rigPath = r'C:\Users\GoodbyeWorld Dev\Documents\Lucille\Tech for Anim\tech-art-exercises\week2\character.mb'.replace("\\", "/")
+    animNs = getFileNamespace(animPath)
+    rigNs = getFileNamespace(rigPath)
+
+    createReference(animPath, animNs)
+    createReference(rigPath, rigNs)
+    
+    maya.cmds.select(cl=True)
+    animJoints = getJointsFromNamespace(animNs)
+    # maya.cmds.select(animJoints)
+    rigJoints = getJointsFromNamespace(rigNs)
+    connectAnimAndRigJoints(animJoints, rigJoints)
+
+    startTime = maya.cmds.playbackOptions(q=True, min=True)
+    endTime = maya.cmds.playbackOptions(q=True, max=True)
+
+    maya.cmds.bakeResults(
+        simulation = True,
+        time = (startTime, endTime),
+        sampleBy = 1,
+        oversamplingRate = 1,
+        disableImplicitControl = True,
+        preserveOutsideKeys = True,
+        sparseAnimCurveBake = False,
+        removeBakedAnimFromLayer = False,
+        bakeOnOverrideLayer = False,
+        minimizeRotation = True,
+        controlPoints = False,
+        shape = True
+    )
+
+    removeReference(animPath)
+
+if __name__ == "__main__":
+    main()
+
